778e2af5946887163748758a7f344c422e11caf7
Your Name <you@example.com>
2022-04-01 11:40:02 +0100

    creating the discord app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+//import { GradientText } from "./components/GradientText";
+
+function App() {
+  return (
+    <div className="">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
